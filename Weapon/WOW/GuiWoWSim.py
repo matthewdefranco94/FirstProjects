ad0da42f32ccfec60106da976d3f41b185112be1
Matthew DeFranco <matthewdefranco94@gmail.com>
2021-04-22 01:58:16 -0700

    Cleaned up DB to WoWsim functions

--- Weapon/WOW/GuiWoWSim.py
+++ Weapon/WOW/GuiWoWSim.py
@@ -15,24 +15,27 @@ instructions = tk.Label(Simulator , text = "This is intended to be a ghetto simu
 instructions.pack()
 
 #Update the listbox
-def update(weapon_query):
+def update(weapon_query_1):
     #Clear the listbox
     weapons_list.delete(0 , END)
     #Add weapons to listbox
-    for items in weapon_query:
-        weapons_list.insert(END , items)
+    # print(type(weapon_query_1))
+    for items in weapon_query_1:
+        # print(type(items))
+        weapons_list.insert(0 , items)
+        
 
 #update entry box with listbox click
 def fillout(event):
     #delete what is in in entry box
     weapon_input.delete(0 , END)
-    #adds the click even from the user
+    #adds the click event from the user
     weapon_input.insert(0 , weapons_list.get(ACTIVE))
 
 #Create an entry box , field that the user inputs
 weapon_label = tk.Label(Simulator , text = 'Enter weapon here: ')
 weapon_label.pack()
-weapon_input = tk.Entry(Simulator)
+weapon_input = tk.Entry(Simulator)   ####user's weapon input
 
 
 # weapon_input.bind('<Enter>' , enter_pressed)
@@ -46,40 +49,47 @@ weapons_list.pack()
 
 #pulls list of weapons user can select
 weapon_query = get_weapons()
-update(weapon_query)
+update(weapon_query)                  ####original location
 
-
-#checks entry against the imported listbox
+#checks entry against the imported listbox  
 def check_listbox(event):
     #get what is user_input
     user_input = weapon_input.get()
+    list_data = []
     if user_input == '':
-        list_data = weapon_query
+        for weapon in weapon_query:
+            list_data.append(weapon)
     else:
-        list_data = []
-        for item in weapon_query:
-            if user_input.lower() in item.lower():
-                list_data.append(item)
+        for weapon in weapon_query:
+            if user_input.lower() in weapon.lower():
+                list_data.append(weapon)
 
     #update listbox with selected item
     update(list_data)
 
 
-#Listboxs = <<>> , single actions = <> , select from the listbox
-weapons_list.bind("<<ListboxSelect>>" , fillout)
-
 #Create binding on entry box to autofill
 weapon_input.bind("<KeyRelease>" , check_listbox)
+
+#Listboxs = <<>> , single actions = <> , select from the listbox
+weapons_list.bind("<<ListboxSelect>>" , fillout)
 
 duration_label = tk.Label(Simulator , text = 'Number of iterations:')
 duration_label.pack()
 duration_label1 = Spinbox(Simulator , from_ = 60 , to_= 600)
 duration_label1.pack()
 
-button = tk.Button(Simulator , text = "Begin Simulation" )
+
+button = tk.Button(Simulator , text = "Begin Simulation" , command = lambda: button_action(weapon_input.get()))
 button.pack()
 
-##### Use pop out window for comparisons
+
+
+def button_action(weapon_input):
+    # print(weapon_input)
+    pass
+
+##### Use pop out window for comparisons?maybe
 
 
 Simulator.mainloop()
---
+++ Weapon/WOW/WoWDBConnector.py
@@ -0,0 +1,47 @@
+import MySQLdb
+import pandas as pd
+import MySQLdb.cursors
+
+#close DB at some point
+db = MySQLdb.connect(host ='localhost', user='root', passwd='password', db='wow_db', cursorclass = MySQLdb.cursors.DictCursor)
+
+cur = db.cursor()
+
+
+
+# user_input = "Lupine Axe"
+def get_weapons():
+    # query = """SELECT entry,name,dmg_min2,dmg_max2,sheath FROM wow_db.item_template where sheath > 0 and name = %s"""
+    query = "SELECT name FROM wow_db.item_template where sheath > 0 ORDER BY name DESC"
+    cur.execute(query)
+    weapons = cur.fetchall()
+    
+    names = []
+    for weapon in weapons:
+        names.append(weapon["name"])
+
+    return names
+
+
+def weapon_stats(weapon_name):
+    query = "SELECT dmg_min1,dmg_min2,delay FROM wow_db.item_template where name = '" + weapon_name + "'"
+    cur.execute(query)
+    
+    return cur.fetchone()
+
+
+# def weapon_dmg():
+#     cur.execute(query)
+#     minimum_damage = cur.fetchall()
+
+#     mindmg = []
+#     for minimum in minimum_damage:
+#         mindmg.append(minimum_damage["dmg_min1"])
+    
+#     return names
+
+
+# shortcut to the above function return
+# return [weapon["name"] for weapon in weapons]
+
+
